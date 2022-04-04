707330a1e5ab07f28cfb023a734da404b1fee216
Kalyan Mishra <55200919+thekalyan001@users.noreply.github.com>
2022-04-04 10:27:23 +0530

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
+import {AiOutlineProject} from 'react-icons/ai'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import { useState } from 'react'
 
@@ -21,7 +21,7 @@ const Nav = () => {
 
     <a href="#experience" onClick={()=>setActiveNav('#experience')} className={activeNav === '#experience'?'active':''}> <BiBook/>  </a>
     
-    <a href="#services" onClick={()=>setActiveNav('#services')} className={activeNav === '#services'?'active':''}> <RiServiceLine/> </a>
+    <a href="#services" onClick={()=>setActiveNav('#portfolio')} className={activeNav === '#portfolio'?'active':''}> <AiOutlineProject/> </a>
     
     <a href="#contact" onClick={()=>setActiveNav('#contact')} className={activeNav === '#contact'?'active':''}> <BiMessageSquareDetail/> </a>
 

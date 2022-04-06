7d6c664dfaa56a249cc1011ea1fc3efcdabaf625
Kalyan Mishra <55200919+thekalyan001@users.noreply.github.com>
2022-04-06 21:43:08 +0530

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,54 +1,54 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
+import IMG2 from '../../assets/portfolio2.JPG'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
+import IMG5 from '../../assets/portfolio5.JPG'
 import IMG6 from '../../assets/portfolio6.jpg'
 
 const data=[
   {
     id: 1,
     image: IMG1,
-    title: 'Cyypto currency Dashboard & financial Visualization',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    title: 'Personal Portfolio',
+    github: 'https://github.com/thekalyan001/KalyanMishra',
+    demo: 'https://kalyanm-portfolio.herokuapp.com/'
   },
   {
     id: 2,
-    image: IMG3,
-    title: 'Chart information & templated in figma',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    image: IMG2,
+    title: 'Accu-Weather Web App',
+    github: 'https://github.com/thekalyan001/Accu-weather',
+    demo: 'https://elated-boyd-d1d86c.netlify.app/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Figma dashboard ui Kit for data design web apps',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    title: 'CityNews Mobile Application',
+    github: 'https://github.com/thekalyan001/CityNews',
+    demo: 'https://youtu.be/Kuj5x30wO6Q'
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Maintaining task and tracking prorgress',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    title: 'ToDo Mobile Application',
+    github: 'https://github.com/thekalyan001/MTodoApp',
+    demo: 'https://github.com/thekalyan001/MTodoApp'
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Chars templates & infographics in figma',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    title: 'Dr. Calculator',
+    github: 'https://github.com/thekalyan001/DrCalculator',
+    demo: 'https://github.com/thekalyan001/DrCalculator'
   },
   {
     id: 1,
-    image: IMG1,
-    title: 'Chary task and tracking pr tempaltes inforgra',
-    github: 'https://github.com',
-    demo: 'https://dribble.com/'
+    image: IMG6,
+    title: 'Vehicle Detection & Counting ML',
+    github: 'https://github.com/thekalyan001/Vehicle_detection_and_counting_project',
+    demo: 'https://github.com/thekalyan001/Vehicle_detection_and_counting_project'
   }
 
 ]
@@ -56,7 +56,7 @@ const Portfolio = () => {
   return (
 <section id="portfolio">
     <h5>My Recent Work</h5>
-    <h2>portfolio</h2>
+    <h2>Projects</h2>
 
     <div className="container portfolio__container">
 
